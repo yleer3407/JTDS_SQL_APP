ac3b82513730d4974b91b5e1c76fd85375830ee7
yleer3407 <312220399@qq.com>
2020-08-11 14:12:31 +0800

    完成二维码扫描识别

--- linkSQLLogin/LinkDatabaseApplication/app/src/main/java/com/example/socket/linkdatabaseapplication/activity/MainActivity.java
+++ linkSQLLogin/LinkDatabaseApplication/app/src/main/java/com/example/socket/linkdatabaseapplication/activity/MainActivity.java
@@ -1,25 +1,39 @@
 package com.example.socket.linkdatabaseapplication.activity;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Handler;
 import android.os.Message;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.socket.linkdatabaseapplication.DbUtil;
 import com.example.socket.linkdatabaseapplication.MyAdapter;
 import com.example.socket.linkdatabaseapplication.R;
+import com.example.socket.linkdatabaseapplication.zxing.android.CaptureActivity;
 
 import static com.example.socket.linkdatabaseapplication.DbUtil.typeLocationList;
 /*
   --------------------------------------数据查询显示在页面上---------------------------------
  */
 public class MainActivity extends AppCompatActivity  {
-
+    private static final String DECODED_CONTENT_KEY = "codedContent";
+    private static final String DECODED_BITMAP_KEY = "codedBitmap";
+    private static final int REQUEST_CODE_SCAN = 0x0000;
     private ListView list;
+    private Button btnscan;
+    private Button btnSearch;
     private final int GETDATA=0;
     private Handler handler=new Handler(){
         @Override
@@ -33,20 +47,79 @@ public class MainActivity extends AppCompatActivity  {
     };
 
 
+//new DownloadDataThread().start();//开启线程
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
-        Button btnSearch = findViewById(R.id.btnTestSql);
-        btnSearch.setOnClickListener(new View.OnClickListener() {
+        btnSearch = findViewById(R.id.btnTestSql);
+        btnscan = findViewById(R.id.btnScan);
+
+        btnscan.setOnClickListener(getClickEvent());
+        btnSearch.setOnClickListener(getClickEvent());
+    }
+
+    private View.OnClickListener getClickEvent() {
+        return new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new DownloadDataThread().start();//开启线程
+                switch (view.getId()){
+                    case R.id.btnScan:
+                        //动态权限申请
+                        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 1);
+                        } else {
+                            goScan();
+                        }
+                        break;
+                    case R.id.btnTestSql:
+                        new DownloadDataThread().start();//开启线程
+                        break;
+                }
             }
-        });
+        };
+    }
 
+    /**
+     * 跳转到扫码界面扫码
+     */
+    private void goScan() {
+        Intent intent = new Intent(MainActivity.this, CaptureActivity.class);
+        startActivityForResult(intent, REQUEST_CODE_SCAN);
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    goScan();
+                } else {
+                    Toast.makeText(this, "你拒绝了权限申请，可能无法打开相机扫码哟！", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
+    }
+    /**
+     *扫码结果返回
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_SCAN && resultCode == RESULT_OK) {
+            if (data != null) {
+                //返回的文本内容
+                String content = data.getStringExtra(DECODED_CONTENT_KEY);
+                //返回的BitMap图像
+                Bitmap bitmap = data.getParcelableExtra(DECODED_BITMAP_KEY);
+                EditText editText = findViewById(R.id.editSearch);
+                editText.setText(content);
+                new DownloadDataThread().start();//开启线程
+               // tv_scanResult.setText("你扫描到的内容是：" + content);
+            }
+        }
     }
 
     private void init() {
